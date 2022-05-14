d1d6860026effdaff02d48a9726dd0a170ce7619
Rodrigo Noal <rodrigonoal@gmail.com>
2022-05-14 15:17:27 -0300

    including list binding adapter

--- app/src/main/java/br/com/siecola/androidproject02/product/ProductBindingAdapters.kt
+++ app/src/main/java/br/com/siecola/androidproject02/product/ProductBindingAdapters.kt
@@ -2,10 +2,19 @@ package br.com.siecola.androidproject02.product
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.siecola.androidproject02.network.Product
 
 //aqui teremos funções para formatar os conteúdos recebidos da API para demonstrá-los na UI
+
+@BindingAdapter("productsList")
+fun bindProductsList(recyclerView: RecyclerView, products: List<Product>?) {
+    val adapter = recyclerView.adapter as ProductAdapter
+    adapter.submitList(products)
+}
 
 @BindingAdapter("productPrice")
 fun bindProductPrice(txtProductPrice: TextView, productPrice: Double?) {
     txtProductPrice.text = productPrice?.let {"$ " + "%.2f".format(it)}
 }
+
