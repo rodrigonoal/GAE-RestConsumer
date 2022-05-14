b6f201ddaf6df23b551e080cea945dcf20629cbd
Rodrigo Noal <rodrigonoal@gmail.com>
2022-05-14 10:23:46 -0300

    json library

--- app/src/main/java/br/com/siecola/androidproject02/network/OauthTokenResponse.kt
+++ app/src/main/java/br/com/siecola/androidproject02/network/OauthTokenResponse.kt
@@ -1,7 +1,11 @@
 package br.com.siecola.androidproject02.network
 
-data class OauthTokenResponse(
-    val accessToken: String,
+import com.squareup.moshi.Json
 
+data class OauthTokenResponse(
+    //neste caso utilizamos a biblioteca para interpretar o json recebido
+    @Json(name = "access_token")
+    val accessToken: String,
+    @Json(name = "expires_in")
     val expiresIn: Int,
 )
